4480d1b1ecf249092f4287393d1a0bbd8e253373
rdhhauzan <muhammad.hauzan@binus.ac.id>
2024-11-20 10:30:01 +0700

    chore(swagger): add swagger documentation

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
       transform: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Reservation Test API')
+    .setDescription('Reservation Test API description')
+    .setVersion('1.0')
+    .addServer('http://localhost:3005', 'Local environment')
+    .addTag('reservation')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, documentFactory);
+
   await app.listen(3005);
 }
 bootstrap();
